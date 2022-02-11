5347a54ad4239d5b747229a2bde389fb9a61d3cb
YohannTisserand <tisserand.yohann@gmail.com>
2022-02-11 01:50:11 +0000

    the temperature can be increased

--- thermostat.js
+++ thermostat.js
@@ -7,6 +7,10 @@ class Thermostat {
   getTemperature() {
     return this.temperature
   }
+
+  up() {
+    this.temperature += 1
+  }
 }
 
 module.exports = Thermostat
--- thermostat.test.js
+++ thermostat.test.js
@@ -6,4 +6,9 @@ describe('Thermostat', () => {
   it('has a defaut value at 20', () => {
     expect(thermostat.getTemperature()).toBe(20);
   });
+
+  it('can raise the temperature', () => {
+    thermostat.up()
+    expect(thermostat.getTemperature()).toBe(21);
+  });
 });
